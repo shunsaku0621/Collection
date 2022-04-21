86787d59a06646307f692bc87b56bf2764aaf0ad
shunsaku0621 <hamagi.0621@icloud.com>
2022-04-21 21:06:23 +0900

    課題提出

---
+++ src/practice/Task.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task {
+	public class Dates{
+//		日付（LocalDate型）
+		List<LocalDate> dates = new ArrayList<>();
+	}
+	
+	public class Contents{
+//		タスク内容（String型）
+		List<String> taskContents = new ArrayList<>();
+	}
+}
